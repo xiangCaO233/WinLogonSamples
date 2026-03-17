4c4910f898e7ddefc11bea9b66d00570aa9197b0
chenruixin <odokoei@outlook.com>
2026-03-17 14:28:31 +0800

    feat: 添加凭证过滤器实现

--- src/samplewrapexistingcredentialprovider/filter/CSampleProviderFilter.h
+++ src/samplewrapexistingcredentialprovider/filter/CSampleProviderFilter.h
@@ -7,19 +7,16 @@ class CSampleFilter : public ICredentialProviderFilter
   public:
     IFACEMETHODIMP Filter(__in CREDENTIAL_PROVIDER_USAGE_SCENARIO cpus, __in DWORD dwFlags,
                           __in_ecount(cProviders) GUID*    rgclsidProviders,
-                          __inout_ecount(cProviders) BOOL* rgbAllow, __in DWORD cProviders) override
-    {
-        for (DWORD i = 0; i < cProviders; i++)
-        {
-            // 屏蔽掉 Windows 原生的密码磁贴
-            // 注意：不影响我们自己在 Provider 内部 CoCreateInstance 加载它！
-            if (rgclsidProviders[i] == CLSID_PasswordCredentialProvider)
-            {
-                rgbAllow[i] = FALSE;
-            }
-        }
-        return S_OK;
-    }
+                          __inout_ecount(cProviders) BOOL* rgbAllow,
+                          __in DWORD                       cProviders) override;
 
     // UpdateRemoteCredential 默认返回 E_NOTIMPL 即可
+    IFACEMETHODIMP UpdateRemoteCredential(
+        /* [annotation][in] */
+        _In_ const CREDENTIAL_PROVIDER_CREDENTIAL_SERIALIZATION* pcpcsIn,
+        /* [annotation][out] */
+        _Out_ CREDENTIAL_PROVIDER_CREDENTIAL_SERIALIZATION* pcpcsOut) override
+    {
+        return E_NOTIMPL;
+    }
 };
